0e77b21734b806c0fe3f9502e8976193e3717aac
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-08 18:30:22 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,12 @@
+// Program check the number even and odd
+#include<stdio.h>
+void main()
+{
+int a ;
+printf(" enter the number",a);
+scanf("%d",&a);
+if( a%2 ==0)
+printf(" entered  no is even%d", a);
+else 
+printf(" entered  no is odd%d", a);
+}
